3a384372270c2b49f1e0fc22cb91466138888b90
Eva Montejano <evamontejano30@gmail.com>
2024-02-19 22:38:27 +0200

    Initial commit

---
+++ BugetTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BugetTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
